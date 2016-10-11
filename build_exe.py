5649573ff85a5d11a212317ce8a922cfb4a8fdc9
AkrISrn <shairkarine@gmail.com>
2016-10-11 16:52:24 +0800

    Create build_exe.py

---
+++ build_exe.py
@@ -0,0 +1,15 @@
+import sys
+from PyInstaller.__main__ import run
+
+bit = "x32"
+if sys.maxsize > (2 ** 31 - 1):
+    bit = "x64"
+
+opts = ['start.py',
+        '-F','-y',
+        '--clean',
+        '--specpath=./spec',
+        '--name=pixdog-' + bit,
+        '--icon=pixdog.ico'
+        ]
+run(opts)
